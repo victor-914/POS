f30d5d9cf8d16191b17dc001197b19d58e56f456
victor <103121630+victor-914@users.noreply.github.com>
2024-05-31 11:02:29 +0100

    new_status

--- src/app/addProduct/components/ProductBar.tsx
+++ src/app/addProduct/components/ProductBar.tsx
@@ -6,29 +6,32 @@ import { Add } from '@mui/icons-material';
 
 export default function ProductBar() {
     return (
-        <Box sx={{ flexGrow: 1, }}>
-            <Typography
-                sx={{
-                    fontSize: "20px",
-                    width: "100%",
-                    textAlign: "center",
-                    lineHeight: "2",
+        <Box sx={{ flexGrow: 1 }}>
 
-                }}
-
-            >Products</Typography>
             <Box
                 sx={{
                     // backgroundColor:'green',
                     display: "flex",
-                    alignItems: 'flex-end',
+                    alignItems: 'center',
                     width: "95%",
                     margin: "auto",
                     flexDirection: 'row',
+                    padding:"10px 5px 10px 5px", 
                     justifyContent: 'space-between'
                 }}
             >
-                <Button variant="contained">
+
+                <Typography
+                    sx={{
+                        fontSize: "20px",
+                        width: "auto",
+                        textAlign: "center",
+                        lineHeight: "2",
+
+                    }}
+
+                >Products</Typography>
+                <Button variant="outlined">
                     <Add />
 
                     <Typography
--- src/app/addProduct/components/ProductListTable.tsx
+++ src/app/addProduct/components/ProductListTable.tsx
@@ -6,53 +6,83 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
+import Link from 'next/router';
+import { useRouter } from 'next/navigation';
 
 function createData(
   name: string,
-  calories: number,
-  fat: number,
-  carbs: number,
-  protein: number,
+  status: string,
+  channel: string,
+  inventory: string,
 ) {
-  return { name, calories, fat, carbs, protein };
+  return { name, status, channel, inventory };
 }
 
 const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
+  createData("shirt", "published", "warehouse", "8 product with 8 variants"),
+  createData("shirt", "published", "warehouse", "8 product with 8 variants"),
+  createData("shirt", "published", "warehouse", "8 product with 8 variants"),
+  createData("shirt", "published", "warehouse", "8 product with 8 variants"),
+  createData("shirt", "published", "warehouse", "8 product with 8 variants"),
+  createData("shirt", "published", "warehouse", "8 product with 8 variants"),
+  createData("shirt", "published", "warehouse", "8 product with 8 variants"),
+  createData("shirt", "published", "warehouse", "8 product with 8 variants"),
+  createData("shirt", "published", "warehouse", "8 product with 8 variants"),
+  createData("shirt", "published", "warehouse", "8 product with 8 variants"),
+  createData("shirt", "published", "warehouse", "8 product with 8 variants"),
+  createData("shirt", "published", "warehouse", "8 product with 8 variants"),
+  createData("shirt", "published", "warehouse", "8 product with 8 variants"),
+  createData("shirt", "published", "warehouse", "8 product with 8 variants"),
+  createData("shirt", "published", "warehouse", "8 product with 8 variants"),
+  createData("shirt", "published", "warehouse", "8 product with 8 variants"),
+  createData("shirt", "published", "warehouse", "8 product with 8 variants"),
+  createData("shirt", "published", "warehouse", "8 product with 8 variants"),
+  createData("shirt", "published", "warehouse", "8 product with 8 variants"),
+  createData("shirt", "published", "warehouse", "8 product with 8 variants"),
+  createData("shirt", "published", "warehouse", "8 product with 8 variants"),
+  createData("shirt", "published", "warehouse", "8 product with 8 variants"),
+  createData("shirt", "published", "warehouse", "8 product with 8 variants"),
+  createData("shirt", "published", "warehouse", "8 product with 8 variants"),
+  createData("shirt", "published", "warehouse", "8 product with 8 variants"),
+
 ];
 
 export default function ProductListTable() {
+ 
+   const router = useRouter()
+
   return (
     <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+      <Table sx={{ minWidth: 650, marginBottom:"50px" }} aria-label="simple table">
         <TableHead>
           <TableRow>
-            <TableCell>Dessert (100g serving)</TableCell>
-            <TableCell align="right">Calories</TableCell>
-            <TableCell align="right">Fat&nbsp;(g)</TableCell>
-            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-            <TableCell align="right">Protein&nbsp;(g)</TableCell>
+            <TableCell>Name</TableCell>
+            <TableCell align="right">Status</TableCell>
+            <TableCell align="right">Channel</TableCell>
+            <TableCell align="right">Inventory</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
           {rows.map((row) => (
             <TableRow
               key={row.name}
+
+              onClick={()=> router.push("/")}
+
               sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
             >
+
               <TableCell component="th" scope="row">
                 {row.name}
               </TableCell>
-              <TableCell align="right">{row.calories}</TableCell>
-              <TableCell align="right">{row.fat}</TableCell>
-              <TableCell align="right">{row.carbs}</TableCell>
-              <TableCell align="right">{row.protein}</TableCell>
+
+              <TableCell align="right">{row.status}</TableCell>
+              <TableCell align="right">{row.channel}</TableCell>
+              <TableCell align="right">{row.inventory}</TableCell>
             </TableRow>
+
           ))}
+
         </TableBody>
       </Table>
     </TableContainer>
